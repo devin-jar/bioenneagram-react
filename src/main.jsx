dbb9033a4985a099e6e7fa273c3a4c916231887b
Juan Camilo Arcila <juanca.arcilav@gmail.com>
2023-05-28 18:07:02 -0500

    improving performance commit 8

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import TagManager from "react-gtm-module";
 
 import { BioenWebSiteApp } from "./BioenWebSiteApp";
 
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const tagManagerArgs = {
-  gtmId: "GTM-KGBLNSF",
-};
-
-TagManager.initialize(tagManagerArgs);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
